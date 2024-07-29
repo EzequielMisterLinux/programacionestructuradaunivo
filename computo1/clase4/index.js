682abf80b93f1d3ab7750b5bce9ce894f61927d6
EzequielMisterLinux <humbertoezequiel.z.c@gmail.com>
2024-07-29 09:39:52 -0600

    agregando ejemplo de funciones al repositorio de gitub

--- computo1/clase4/index.js
+++ computo1/clase4/index.js
@@ -2,12 +2,29 @@ import leerlinea from "readline"
 
 const interfaz = leerlinea.createInterface(
     process.stdin,
-
     process.stdout
 )
 
+const NewUser = () => {
+    interfaz.question("whats is your name: " , (name) => {
+        interfaz.question("whats is your middlename: ", (middlename) => {
+            interfaz.question("whats is your age: ", (age) => {
+                if (age >= 18) {
+                    console.log(`your fullname is : ${name} ${middlename} your age is ${age}`)
+                    console.log("youre age is hight")
+                    interfaz.close()
+                    
+                }else{
+                    console.log(`your fullname is : ${name} ${middlename} your age is ${age}`)
+                    console.log("you are age is low");
+                    
+                    interfaz.close()
+                }
+                
+            })
+        })
+    })
 
-interfaz.question("whats is your age", (asw)=> {
-    console.log("lol", asw);
-    
-})
+}
+
+NewUser()
