6ffd1364d8b2954f57d0ef815def5a8110cec816
EzequielMisterLinux <humbertoezequiel.z.c@gmail.com>
2024-07-31 10:29:03 -0600

    agregando ejercicio de cadena a mayuscula

---
+++ computo1/clase5/cadenaupper.js
@@ -0,0 +1,15 @@
+
+const TransformToMayus = () => {
+    
+    let valor = "fdff"
+    return valor.toUpperCase()
+}
+
+function mayus(){
+    let value = "fuhfuhgdu"
+    return value.toUpperCase()
+}
+
+console.log( TransformToMayus());
+
+console.log(mayus());
--- computo1/clase5/ejercicio1.js
+++ computo1/clase5/ejercicio1.js
@@ -15,7 +15,6 @@ const Sumar = (num1, num2) => {
 
 }
 
-
 const resta = (num1, num2) => {
     return num1 - num2
 }
