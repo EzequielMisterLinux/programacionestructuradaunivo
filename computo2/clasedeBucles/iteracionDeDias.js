ddfee8bcf6e056a2caade5f74ff7220fe644113b
EzequielMisterLinux <humbertoezequiel.z.c@gmail.com>
2024-09-25 16:08:10 -0600

    agregando tarea de bucles basicos

---
+++ computo2/clasedeBucles/iteracionDeDias.js
@@ -0,0 +1,17 @@
+
+
+const dias = [
+    "lunes",
+    "martes",
+    "miercoles",
+    "jueves",
+    "viernes",
+    "sabado",
+    "domingo"
+]
+
+
+for (let dia of dias){
+    console.log(dia);
+    
+}
---
+++ computo2/clasedeBucles/iteracionDeHelloWorld.js
@@ -0,0 +1,7 @@
+let hello = "Hola mundo"
+
+
+for(let i = 0; i < 5 ; i++){
+    console.log(hello)
+    
+}
